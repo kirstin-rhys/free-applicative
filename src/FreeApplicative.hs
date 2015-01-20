4f37995634d430d4dac49b9622f3f690e8f44627
kirstin penelope rhys <kirstin.rhys@gmail.com>
2015-01-19 22:41:09 -0800

    Initial code import

---
+++ src/FreeApplicative.hs
@@ -0,0 +1,32 @@
+{-# LANGUAGE GADTs, NoImplicitPrelude, UnicodeSyntax #-}
+
+module FreeApplicative (Free(..)) where
+
+import Data.Function (($), flip)
+
+class Functor φ where
+  (<$>) ∷ (α → β) → φ α → φ β
+
+class Functor φ ⇒ Applicative φ where
+  pure ∷ α               → φ α
+  (⊛)  ∷ φ (α → β) → φ α → φ β
+
+class Applicative φ => Monad φ where
+  (>>=) ∷ φ α → (α → φ β) → φ β
+
+data Free φ α where
+     V ∷ α            → Free φ α
+     N ∷ φ (Free φ α) → Free φ α
+
+instance Functor φ ⇒ Functor (Free φ) where
+  g <$> (V x) = V $ g x
+  g <$> (N h) = N $ (g <$>) <$> h
+
+instance Functor φ => Applicative (Free φ) where
+  pure = V
+  (V x) ⊛ f = x <$> f
+  (N g) ⊛ f = N $ (flip ($) <$> f ⊛) <$> g
+
+instance Functor φ => Monad (Free φ) where
+  (V x) >>= k = k x
+  (N g) >>= k = N $ (>>= k) <$> g
